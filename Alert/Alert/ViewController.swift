7b9f2bf7f792b727fe4332970a09b591bad87c59
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-10-06 23:15:19 +0900

    alert project
    guitarShop

--- Alert/Alert/ViewController.swift
+++ Alert/Alert/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     }
     @IBAction func btnTelecaster(_ sender: Any) {
         if(guitar.image == tele){
-            let lampOnAlert = UIAlertController(title: "경고", message: "현재 Telecaster 상태입니다.", preferredStyle: UIAlertController.Style.alert)
+            let lampOnAlert = UIAlertController(title: "경고", message: "현재 Telecaster입니다.", preferredStyle: UIAlertController.Style.alert)
             
             let OnAction = UIAlertAction(title: "네 알겠습니다", style: UIAlertAction.Style.default, handler: nil)
             lampOnAlert.addAction(OnAction)
@@ -38,9 +38,84 @@ class ViewController: UIViewController {
             //guitarTele = true
         }
     }
-    @IBAction func btnLespaul(_ sender: Any) {
+   
+ 
+    @IBAction func btnLespaul(_ sender: UIButton) {
+        if(guitar.image == lp){
+            let lampOnAlert = UIAlertController(title: "경고", message: "현재 lesspaul입니다.", preferredStyle: UIAlertController.Style.alert)
+            
+            let OnAction = UIAlertAction(title: "네 알겠습니다", style: UIAlertAction.Style.default, handler: nil)
+            lampOnAlert.addAction(OnAction)
+            present(lampOnAlert, animated: true, completion: nil)
+        }
+        else{
+            guitar.image = lp
+            //guitarTele = true
+        }
     }
+    
+    
+    
     @IBAction func btnJazzmaster(_ sender: Any) {
+        if(guitar.image == jm){
+            let lampOnAlert = UIAlertController(title: "경고", message: "현재 jazzmaster입니다.", preferredStyle: UIAlertController.Style.alert)
+            
+            let OnAction = UIAlertAction(title: "네 알겠습니다", style: UIAlertAction.Style.default, handler: nil)
+            lampOnAlert.addAction(OnAction)
+            present(lampOnAlert, animated: true, completion: nil)
+        }
+        else{
+            guitar.image = jm
+            //guitarTele = true
+        }
+    }
+    
+    
+    @IBAction func btnHome(_ sender: Any) {
+        if(guitar.image == jm){
+            let goHome = UIAlertController(title: "홈으로 가기", message: "홈으로 가시겠습니까?", preferredStyle: UIAlertController.Style.alert)
+            
+            let homeAction = UIAlertAction(title: "네", style: UIAlertAction.Style.default,handler: {
+                ACTION in self.guitar.image = self.guitarshop
+            })
+            
+            let cancelAction = UIAlertAction(title: "아니오", style: UIAlertAction.Style.default, handler: nil)
+            
+            goHome.addAction(homeAction)
+            goHome.addAction(cancelAction)
+            
+            present(goHome, animated: true, completion: nil)
+        }
+        
+        if(guitar.image == lp){
+            let goHome = UIAlertController(title: "램프끄기", message: "홈으로 가시겠습니까?", preferredStyle: UIAlertController.Style.alert)
+            
+            let homeAction = UIAlertAction(title: "네", style: UIAlertAction.Style.default,handler: {
+                ACTION in self.guitar.image = self.guitarshop
+            })
+            
+            let cancelAction = UIAlertAction(title: "아니오", style: UIAlertAction.Style.default, handler: nil)
+            
+            goHome.addAction(homeAction)
+            goHome.addAction(cancelAction)
+            
+            present(goHome, animated: true, completion: nil)
+        }
+        
+        if(guitar.image == tele){
+            let goHome = UIAlertController(title: "램프끄기", message: "홈으로 가시겠습니까?", preferredStyle: UIAlertController.Style.alert)
+            
+            let homeAction = UIAlertAction(title: "네", style: UIAlertAction.Style.default,handler: {
+                ACTION in self.guitar.image = self.guitarshop
+            })
+            
+            let cancelAction = UIAlertAction(title: "아니오", style: UIAlertAction.Style.default, handler: nil)
+            
+            goHome.addAction(homeAction)
+            goHome.addAction(cancelAction)
+            
+            present(goHome, animated: true, completion: nil)
+        }
     }
     
 
